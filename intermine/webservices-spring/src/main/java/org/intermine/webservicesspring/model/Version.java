259227030f7a5dda9996ef840b584cf2e6d6ea03
PKatGITHUB <prabodh.kotasthane@gmail.com>
2019-03-24 21:58:30 +0530

    Added webservices-spring module.

---
+++ intermine/webservices-spring/src/main/java/org/intermine/webservicesspring/model/Version.java
@@ -0,0 +1,174 @@
+package org.intermine.webservicesspring.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Version
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-03-23T11:29:25.537+05:30[Asia/Kolkata]")
+public class Version   {
+  @JsonProperty("version")
+  private Integer version = null;
+
+  @JsonProperty("executionTime")
+  private String executionTime = null;
+
+  @JsonProperty("wasSuccessful")
+  private Boolean wasSuccessful = null;
+
+  @JsonProperty("error")
+  private String error = null;
+
+  @JsonProperty("statusCode")
+  private Integer statusCode = null;
+
+  public Version version(Integer version) {
+    this.version = version;
+    return this;
+  }
+
+  /**
+   * Get version
+   * @return version
+  **/
+  @ApiModelProperty(value = "")
+
+  public Integer getVersion() {
+    return version;
+  }
+
+  public void setVersion(Integer version) {
+    this.version = version;
+  }
+
+  public Version executionTime(String executionTime) {
+    this.executionTime = executionTime;
+    return this;
+  }
+
+  /**
+   * Get executionTime
+   * @return executionTime
+  **/
+  @ApiModelProperty(value = "")
+
+  public String getExecutionTime() {
+    return executionTime;
+  }
+
+  public void setExecutionTime(String executionTime) {
+    this.executionTime = executionTime;
+  }
+
+  public Version wasSuccessful(Boolean wasSuccessful) {
+    this.wasSuccessful = wasSuccessful;
+    return this;
+  }
+
+  /**
+   * Get wasSuccessful
+   * @return wasSuccessful
+  **/
+  @ApiModelProperty(value = "")
+
+  public Boolean isWasSuccessful() {
+    return wasSuccessful;
+  }
+
+  public void setWasSuccessful(Boolean wasSuccessful) {
+    this.wasSuccessful = wasSuccessful;
+  }
+
+  public Version error(String error) {
+    this.error = error;
+    return this;
+  }
+
+  /**
+   * Get error
+   * @return error
+  **/
+  @ApiModelProperty(value = "")
+
+  public String getError() {
+    return error;
+  }
+
+  public void setError(String error) {
+    this.error = error;
+  }
+
+  public Version statusCode(Integer statusCode) {
+    this.statusCode = statusCode;
+    return this;
+  }
+
+  /**
+   * Get statusCode
+   * @return statusCode
+  **/
+  @ApiModelProperty(value = "")
+
+  public Integer getStatusCode() {
+    return statusCode;
+  }
+
+  public void setStatusCode(Integer statusCode) {
+    this.statusCode = statusCode;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Version version = (Version) o;
+    return Objects.equals(this.version, version.version) &&
+        Objects.equals(this.executionTime, version.executionTime) &&
+        Objects.equals(this.wasSuccessful, version.wasSuccessful) &&
+        Objects.equals(this.error, version.error) &&
+        Objects.equals(this.statusCode, version.statusCode);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(version, executionTime, wasSuccessful, error, statusCode);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Version {\n");
+    
+    sb.append("    version: ").append(toIndentedString(version)).append("\n");
+    sb.append("    executionTime: ").append(toIndentedString(executionTime)).append("\n");
+    sb.append("    wasSuccessful: ").append(toIndentedString(wasSuccessful)).append("\n");
+    sb.append("    error: ").append(toIndentedString(error)).append("\n");
+    sb.append("    statusCode: ").append(toIndentedString(statusCode)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
